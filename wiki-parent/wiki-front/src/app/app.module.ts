3034ded0c52e0d686b87755d1b6b28a8d665ab9d
Turbo Xav <xavier.tagliarino@gmail.com>
2018-07-27 12:07:47 +0200

    ajout bouton

--- wiki-parent/wiki-front/src/app/app.module.ts
+++ wiki-parent/wiki-front/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthentificationComponent } from './header/authentification/authentification.component';
 import { GuidelineComponent } from './guideline/guideline.component';
 import { ArchitectureTeamComponent } from './architecture-team/architecture-team.component';
 import { RefServiceComponent } from './ref-service/ref-service.component';
 import { FooterComponent } from './footer/footer.component';
-import { AuthComponent } from './auth/auth.component';
 
 
 @NgModule({
@@ -19,7 +18,7 @@ import { AuthComponent } from './auth/auth.component';
     ArchitectureTeamComponent,
     RefServiceComponent,
     FooterComponent,
-    AuthComponent
+    AuthentificationComponent
   ],
   imports: [
     BrowserModule
