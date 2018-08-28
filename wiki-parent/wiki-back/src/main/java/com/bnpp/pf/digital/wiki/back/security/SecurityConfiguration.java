de78ebf46c6542fbf50f2d8e66ddf4969a52f835
Turbo Xav <xavier.tagliarino@gmail.com>
2018-08-28 17:04:17 +0200

    merge avec security et crud guideline

--- wiki-parent/wiki-back/src/main/java/com/bnpp/pf/digital/wiki/back/security/SecurityConfiguration.java
+++ wiki-parent/wiki-back/src/main/java/com/bnpp/pf/digital/wiki/back/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -60,7 +61,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.exceptionHandling().accessDeniedPage("/rest/auth/access-denied")
 		.and()		
 		.logout().logoutUrl("/rest/auth/logout")
-		.logoutSuccessUrl("/rest/auth/access-denied")
+		.logoutSuccessUrl("/rest/role")
 		.deleteCookies("auth_code", "JSESSIONID")
 		.addLogoutHandler(logoutHandler)
 		.clearAuthentication(true)
