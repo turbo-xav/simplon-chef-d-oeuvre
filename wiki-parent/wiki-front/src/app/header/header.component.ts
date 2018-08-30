b6f1e1a12ca2799a46a4ccae9686a188c490ad71
Emma Contardo <emmanuelle.contardo.pf1@gmail.com>
2018-08-30 10:28:42 +0200

    Affichage menu

--- wiki-parent/wiki-front/src/app/header/header.component.ts
+++ wiki-parent/wiki-front/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../menu.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public menuService: MenuService) { }
 
   ngOnInit() {
   }
-
+  selectMenu (name) {
+    this.menuService.selectedMenu = name;
+  }
 }
--- wiki-parent/wiki-front/src/app/menu.service.ts
+++ wiki-parent/wiki-front/src/app/menu.service.ts
@@ -2,7 +2,7 @@ import { Menu } from './menu';
 import { Injectable } from '@angular/core';
 
 
-
+@Injectable()
 export class MenuService {
 
   constructor() {
