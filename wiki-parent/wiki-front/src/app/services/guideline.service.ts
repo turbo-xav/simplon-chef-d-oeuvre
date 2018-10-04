da23cdad15d0273cd1f2e5c2d9ad115de6f1a036
Emma Contardo <emmanuelle.contardo.pf1@gmail.com>
2018-10-04 11:10:47 +0200

    design

--- wiki-parent/wiki-front/src/app/services/guideline.service.ts
+++ wiki-parent/wiki-front/src/app/services/guideline.service.ts
@@ -16,8 +16,8 @@ export class GuidelineService {
     const formData = new FormData();
     formData.append('file', fileToUpload);
     console.log(JSON.stringify(guideline));
-    formData.append('guideline',  JSON.stringify(guideline));
-    console.log('guideline',   guideline);
+    formData.append('guideline', JSON.stringify(guideline));
+    console.log('guideline', guideline);
     return this.http.post<Observable<any>>(this.restUrl + '/upload', formData);
 
     // const headers = new HttpHeaders().append('Content-Type', 'multipart/form-data');
